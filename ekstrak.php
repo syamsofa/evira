5009e5df01c2e2b9e818ce9322480bc066937c9c
achiruzaman <achiruzaman>
2022-12-03 14:28:27 +0700

    UU

--- ekstrak.php
+++ ekstrak.php
@@ -1,5 +1,5 @@
 <?php
-if (($open = fopen("data.csv", "r")) !== FALSE) {
+if (($open = fopen("datas.csv", "r")) !== FALSE) {
 
     while (($data = fgetcsv($open, 1000, ";")) !== FALSE) {
         $array[] = $data;
@@ -9,7 +9,6 @@ if (($open = fopen("data.csv", "r")) !== FALSE) {
 }
 echo "<pre>";
 //To display array data
-print_r($array[1]);
 echo "</pre>";
 
 $servername = "localhost";
@@ -30,9 +29,18 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row
     while ($row = $result->fetch_assoc()) {
+
         // echo $row["Id"]."\n";
+        // $conn->query("update sls set NomorBatch=? where KdKec=? and KdDesa=? and KdSls=?",[]);
     }
 } else {
     echo "0 results";
+}
+print_r($array[0]);
+
+foreach ($array as $arr) {
+    // print_r($arr);
+    $conn->query("update sls set NomorBatch=".$arr[6]." ,Vk1=".$arr[7].",Vk2=".$arr[8].",K=".$arr[9].",Xk=".$arr[10].",Psls=".$arr[11].",PetaWs=".$arr[12].",Banr=".$arr[13].",HasilVerifikasi=".$arr[14].",IsPercepatan=".$arr[15]." where KdKec=".$arr[0]." and KdDesa=".$arr[1]." and KdSls=".$arr[2]."");
+    # code...
 }
 $conn->close();
